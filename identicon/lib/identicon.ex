95de74b34e9da347d6101d7fedb2da52f41535c6
RingoChui <ringo.chui@thescore.com>
2023-03-23 13:39:06 -0400

    updated identicon

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -15,4 +15,21 @@ defmodule Identicon do
   def hello do
     :world
   end
+
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color do
+
+  end
+
 end
