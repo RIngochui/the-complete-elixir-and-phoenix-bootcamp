3c39562fac20d396292844aa24169e68644879ee
RIngochui <ringochewyis@gmail.com>
2023-03-23 22:29:12 -0400

    worked on identicon methods

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -20,6 +20,39 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn {code, _index} ->
+       rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirrow_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirrow_row(row) do
+    [first, second, third] = row
+    [first, second, third, second, first]
+
+    # [first, second | _tail] = row
+    # row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
@@ -27,11 +60,6 @@ defmodule Identicon do
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
-  end
-
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    %Identicon.Image{image | color: {r, g, b}}
   end
 
 end
